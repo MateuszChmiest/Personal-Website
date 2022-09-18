d2b9131302b31277f31c3c5053bafe405d196634
Mateusz Chmiest <mateusz_269@icloud.com>
2022-09-18 20:00:15 +0200

    styling about me section done

--- my-app/src/components/AboutMe/AboutMe.js
+++ my-app/src/components/AboutMe/AboutMe.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import MyPhoto from "../../asstes/myPhoto.JPG"
+import Typewriter from "typewriter-effect";
 
 const AboutMe = () => {
-  return (
-    <div className={"w-full h-screen"}>AboutMe</div>
-  )
-}
+	return (
+		<div className={"w-full h-screen flex justify-center items-center"}>
+			<div
+				className={
+					"container h-full mx-auto flex flex-col justify-center items-center"
+				}>
+				<div className={"grid grid-cols-2 gap-8 w-full"}>
+					<div className={"text-right pb-8 pl-4"}>
+						<p
+							className={"text-3xl md:text-4xl text-primary inline border-b-2 border-blackColor"}>
+							About Me
+						</p>
+					</div>
+					<div></div>
+				</div>
+				<div className={"container grid grid-cols-2 gap-8"}>
+					<div className={"ml-2"}>
+						<img className={"max-w-[280px] w-full float-right"} src={MyPhoto} alt="Mateusz photo"/>
+					</div>
+					<div className={"mr-2"}>
+						<p className={"text-[11px] leading-3 sm:text-sm md:leading-8 md:text-xl max-w-xs"}>I started my adventure with programming in December 2021, on a course at the IT school "Coders Lab". In programming, I am fascinated by the possibility of creating, constant development and the end result of my work. I'm an ambitious person with a great willingness to learn and develop.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default AboutMe
+export default AboutMe;
