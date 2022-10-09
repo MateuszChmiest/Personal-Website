5e507c95bbfa812548b0f288e16eb432d1314ae0
Mateusz Chmiest <mateusz_269@icloud.com>
2022-10-09 21:29:16 +0200

    update

--- my-app/src/components/Contact/Contact.js
+++ my-app/src/components/Contact/Contact.js
@@ -4,41 +4,55 @@ import Typewriter from "typewriter-effect";
 
 const Contact = () => {
 	return (
-			<div
-				className={
-					"h-full w-full xl:min-h-[1200px] bg-backgroundColor flex justify-center items-center relative gradient"
-				}>
-				<form className={"container flex flex-col items-center justify-center z-50"}>
-					<div className={"pb-8"}>
-						<p>Contact</p>
-						<p
-							className={"text-base xl:text-lgl text-secondary text-opacity-60"}>
-							<Typewriter
-								options={{
-									deleteSpeed: 50,
-									loop: true,
-									delay: 90
-								}}
-								onInit={(typewriter) => {
-									typewriter
-										.typeString("I'm a Frontend Developer.")
-										.pauseFor(1000)
-										.deleteAll()
-										.typeString("Check my portfolio!")
-										.start();
-								}}
-							/>
-						</p>
-					</div>
-					<input className={"bg-secondary p-2 w-1/2"} type="text" placeholder="Name" name="name"/>
-					<input className={"bg-secondary p-2 my-4 w-1/2"} type="email" placeholder="Email" name="email"/>
-					<textarea className={"bg-secondary p-2 w-1/2"} name="message" rows="10" placeholder="Your message"></textarea>
-					<button type="submit">okej</button>
-				</form>
-				<div className={"absolute -top-1 w-full z-10"}>
-					<FooterWave />
+		<div
+			className={
+				"h-full w-full min-h-[1000px] xl:min-h-[1300px] bg-backgroundColor flex justify-center items-center relative gradient"
+			}>
+			<form
+				className={"container flex flex-col items-center justify-center z-50"}>
+				<div className={"pb-8"}>
+					<p>Contact</p>
+					<p className={"text-base xl:text-lgl text-secondary text-opacity-60"}>
+						<Typewriter
+							options={{
+								deleteSpeed: 50,
+								loop: true,
+								delay: 90,
+							}}
+							onInit={(typewriter) => {
+								typewriter
+									.typeString("I'm a Frontend Developer.")
+									.pauseFor(1000)
+									.deleteAll()
+									.typeString("Check my portfolio!")
+									.start();
+							}}
+						/>
+					</p>
 				</div>
+				<input
+					className={"bg-secondary p-2 w-1/2"}
+					type='text'
+					placeholder='Name'
+					name='name'
+				/>
+				<input
+					className={"bg-secondary p-2 my-4 w-1/2"}
+					type='email'
+					placeholder='Email'
+					name='email'
+				/>
+				<textarea
+					className={"bg-secondary p-2 w-1/2"}
+					name='message'
+					rows='10'
+					placeholder='Your message'></textarea>
+				<button type='submit'>okej</button>
+			</form>
+			<div className={"absolute -top-1 w-full z-10"}>
+				<FooterWave />
 			</div>
+		</div>
 	);
 };
 
